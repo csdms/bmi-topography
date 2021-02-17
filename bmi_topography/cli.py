5b8f1e358fce374e0b69eafdf3f330081fd7c633
Mark Piper <mark.piper@colorado.edu>
2021-02-17 13:01:41 -0700

    Set up CLI parameters

--- bmi_topography/cli.py
+++ bmi_topography/cli.py
@@ -6,6 +6,54 @@ from .topography import Topography
 
 @click.command()
 @click.version_option()
-def main():
+@click.option(
+    "--dem_type",
+    type=click.Choice(["SRTMGL3", "SRTMGL1", "SRTMGL1_E"], case_sensitive=False),
+    default="SRTMGL3",
+    help="The global raster dataset.",
+    show_default="SRTMGL3",
+)
+@click.option(
+    "--south",
+    type=click.FloatRange(-90, 90),
+    default=36.738884,
+    help="WGS 84 bounding box south coordinate, in degrees, on [-90,90].",
+    show_default=36.738884,
+)
+@click.option(
+    "--north",
+    type=click.FloatRange(-90, 90),
+    default=38.091337,
+    help="WGS 84 bounding box north coordinate, in degrees, on [-90,90].",
+    show_default=38.091337,
+)
+@click.option(
+    "--west",
+    type=click.FloatRange(-180, 180),
+    default=-120.168457,
+    help="WGS 84 bounding box west coordinate, in degrees, on [-180,180].",
+    show_default=-120.168457,
+)
+@click.option(
+    "--east",
+    type=click.FloatRange(-180, 180),
+    default=-118.465576,
+    help="WGS 84 bounding box east coordinate, in degrees, on [-180,180].",
+    show_default=-118.465576,
+)
+@click.option(
+    "--output_format",
+    type=click.Choice(["GeoTiff", "AAIGrid", "HFA"], case_sensitive=False),
+    default="GeoTiff",
+    help="Output file format.",
+    show_default="GeoTiff",
+)
+def main(dem_type, south, north, west, east, output_format):
     """Fetch and cache Shuttle Radar Topography Mission (SRTM) elevation data"""
     Topography.run()
+    print(dem_type)
+    print(south)
+    print(north)
+    print(west)
+    print(east)
+    print(output_format)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,12 +4,105 @@ from click.testing import CliRunner
 from bmi_topography.cli import main
 
 
-def test_command_line_interface():
+def test_help():
     runner = CliRunner()
     result = runner.invoke(main, ["--help"])
     assert result.exit_code == 0
     assert "Usage:" in result.output
 
+
+def test_version():
+    runner = CliRunner()
     result = runner.invoke(main, ["--version"])
     assert result.exit_code == 0
     assert "version" in result.output
+
+
+def test_defaults():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 0
+
+
+def test_demtype_valid():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--dem_type=SRTMGL1"])
+    assert result.exit_code == 0
+
+
+def test_demtype_invalid():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--dem_type=foobar"])
+    assert result.exit_code != 0
+
+
+def test_demtype_is_case_insensitive():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--dem_type=srtmgl1"])
+    assert result.exit_code == 0
+
+
+def test_south_inrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--south=40.0"])
+    assert result.exit_code == 0
+
+
+def test_south_outrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--south=400.0"])
+    assert result.exit_code != 0
+
+
+def test_north_inrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--north=20.0"])
+    assert result.exit_code == 0
+
+
+def test_north_outrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--north=200.0"])
+    assert result.exit_code != 0
+
+
+def test_west_inrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--west=-30.0"])
+    assert result.exit_code == 0
+
+
+def test_west_outrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--west=-300.0"])
+    assert result.exit_code != 0
+
+
+def test_east_inrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--east=50.0"])
+    assert result.exit_code == 0
+
+
+def test_east_outrange():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--east=-500.0"])
+    assert result.exit_code != 0
+
+
+def test_output_format_valid():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--output_format=GeoTiff"])
+    assert result.exit_code == 0
+
+
+def test_output_format_invalid():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--output_format=foobar"])
+    assert result.exit_code != 0
+
+
+def test_output_format_is_case_insensitive():
+    runner = CliRunner()
+    result = runner.invoke(main, ["--output_format=geotiff"])
+    assert result.exit_code == 0

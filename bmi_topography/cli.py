0b050a1713cede143b6fe4ce7cf942563e050921
mcflugen <mcflugen@gmail.com>
2022-01-24 21:17:38 -0700

    add a teeny bit of documentation about api keys

--- bmi_topography/cli.py
+++ bmi_topography/cli.py
@@ -51,7 +51,20 @@ from .topography import Topography
 )
 @click.option("--no_fetch", is_flag=True, help="Do not fetch data from server.")
 def main(quiet, dem_type, south, north, west, east, output_format, no_fetch):
-    """Fetch and cache NASA SRTM and JAXA ALOS land elevation data"""
+    """Fetch and cache NASA SRTM and JAXA ALOS land elevation data
+
+    To fetch some datasets you will need an OpenTopography API key.
+    You can find instructions on how to obtain one from the OpenTopography
+    website:
+
+        https://opentopography.org/blog/introducing-api-keys-access-opentopography-global-datasets
+
+    Once you have received your key, you can pass it to the *bmi-topography*
+    command in one of two ways:
+    1. As the environment variable, OPEN_TOPOGRAPHY_API_KEY.
+    2. As the contents of an *.open_topography.txt* file located either in
+       your current directory or you home directory.
+    """
     topo = Topography(dem_type, south, north, west, east, output_format)
     if not no_fetch:
         if not quiet:

c6c99b8c4a63a00198de77b73b5d022793ec1c6e
Eric Hutton <mcflugen@users.noreply.github.com>
2022-01-25 12:02:30 -0700

    Use an API key when requesting OpenTopography data (#25)
    * add api_key keyword to Topography
    
    * ignore .open_topography.txt file
    
    * return empty string rather than None for file contents
    
    * add OpenTopography API key to the test action
    
    * add unit tests for finding the api key
    
    * Add contributors
    
    * add a teeny bit of documentation about api keys
    
    * Use opentopography in place of open_topography
    
    Squished together is closer to the org name.
    
    Co-authored-by: Mark Piper <mark.piper@colorado.edu>

---
+++ tests/test_api_key.py
@@ -0,0 +1,58 @@
+import os
+from unittest import mock
+
+from bmi_topography.topography import find_api_key, read_first_of
+
+
+def copy_environ(exclude=None):
+    if exclude is None:
+        exclude = {}
+    elif isinstance(exclude, str):
+        exclude = {exclude}
+
+    return {key: value for key, value in os.environ.items() if key not in exclude}
+
+
+def test_find_api_key_not_found():
+    """The API key is not given anywhere"""
+    env = copy_environ(exclude="OPENTOPOGRAPHY_API_KEY")
+    with mock.patch.dict(os.environ, env, clear=True):
+        assert find_api_key() == ""
+
+
+@mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
+def test_find_api_key_env(tmpdir):
+    """The API key is an environment variable"""
+    with tmpdir.as_cwd():
+        with open(".opentopography.txt", "w") as fp:
+            fp.write("bar")
+    assert find_api_key() == "foo"
+
+
+@mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
+def test_find_api_key_from_file(tmpdir):
+    """The API key is in a file"""
+    env = copy_environ(exclude="OPENTOPOGRAPHY_API_KEY")
+    with tmpdir.as_cwd():
+        with open(".opentopography.txt", "w") as fp:
+            fp.write("bar")
+
+        with mock.patch.dict(os.environ, env, clear=True):
+            assert find_api_key() == "bar"
+
+
+def test_read_first_missing(tmpdir):
+    with tmpdir.as_cwd():
+        assert read_first_of(["foo.txt"]) == ""
+        assert read_first_of([]) == ""
+
+
+def test_read_first_file(tmpdir):
+    with tmpdir.as_cwd():
+        with open("foo.txt", "w") as fp:
+            fp.write("foo")
+        with open("bar.txt", "w") as fp:
+            fp.write("bar")
+
+        assert read_first_of(["foo.txt", "bar.txt"]) == "foo"
+        assert read_first_of(["bar.txt", "foo.txt"]) == "bar"

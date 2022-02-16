f5d20be82416a7ac92af4e129bfb6c7039eceb81
Eric Hutton <mcflugen@users.noreply.github.com>
2022-02-16 14:15:50 -0700

    feat: use a demo API key if a user key can't be found (#34)
    * use a demo API key if a user key can't be found
    
    * rename find_api_key, find_user_api_key
    
    * add the demo key
    
    * remove example .opentopography.txt file
    
    * add a couple tests for use_demo_key

--- tests/test_api_key.py
+++ tests/test_api_key.py
@@ -1,7 +1,9 @@
 import os
 from unittest import mock
 
-from bmi_topography.topography import find_api_key, read_first_of
+import pytest
+
+from bmi_topography.topography import find_user_api_key, read_first_of, use_demo_key
 
 
 def copy_environ(exclude=None):
@@ -13,24 +15,24 @@ def copy_environ(exclude=None):
     return {key: value for key, value in os.environ.items() if key not in exclude}
 
 
-def test_find_api_key_not_found():
+def test_find_user_api_key_not_found():
     """The API key is not given anywhere"""
     env = copy_environ(exclude="OPENTOPOGRAPHY_API_KEY")
     with mock.patch.dict(os.environ, env, clear=True):
-        assert find_api_key() == ""
+        assert find_user_api_key() == ""
 
 
 @mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
-def test_find_api_key_env(tmpdir):
+def test_find_user_api_key_env(tmpdir):
     """The API key is an environment variable"""
     with tmpdir.as_cwd():
         with open(".opentopography.txt", "w") as fp:
             fp.write("bar")
-    assert find_api_key() == "foo"
+    assert find_user_api_key() == "foo"
 
 
 @mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
-def test_find_api_key_from_file(tmpdir):
+def test_find_user_api_key_from_file(tmpdir):
     """The API key is in a file"""
     env = copy_environ(exclude="OPENTOPOGRAPHY_API_KEY")
     with tmpdir.as_cwd():
@@ -38,7 +40,7 @@ def test_find_api_key_from_file(tmpdir):
             fp.write("bar")
 
         with mock.patch.dict(os.environ, env, clear=True):
-            assert find_api_key() == "bar"
+            assert find_user_api_key() == "bar"
 
 
 def test_read_first_missing(tmpdir):
@@ -56,3 +58,14 @@ def test_read_first_file(tmpdir):
 
         assert read_first_of(["foo.txt", "bar.txt"]) == "foo"
         assert read_first_of(["bar.txt", "foo.txt"]) == "bar"
+
+
+def test_use_demo_key_is_a_string():
+    demo_key = use_demo_key()
+    assert isinstance(demo_key, str)
+    assert len(demo_key) > 0
+
+
+def test_use_demo_key_issues_warning():
+    with pytest.warns(UserWarning):
+        use_demo_key()

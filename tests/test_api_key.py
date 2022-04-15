e9669ad7f16d6f1f95e2ff27a3489c5c0afb10c0
mcflugen <mcflugen@gmail.com>
2022-04-14 18:26:00 -0600

    update unit tests for ApiKey

--- tests/test_api_key.py
+++ tests/test_api_key.py
@@ -3,7 +3,7 @@ from unittest import mock
 
 import pytest
 
-from bmi_topography.topography import find_user_api_key, read_first_of, use_demo_key
+from bmi_topography.api_key import ApiKey, find_first_of
 
 
 def copy_environ(exclude=None):
@@ -19,7 +19,8 @@ def test_find_user_api_key_not_found():
     """The API key is not given anywhere"""
     env = copy_environ(exclude="OPENTOPOGRAPHY_API_KEY")
     with mock.patch.dict(os.environ, env, clear=True):
-        assert find_user_api_key() == ""
+        with pytest.raises(ValueError):
+            ApiKey.from_env()
 
 
 @mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
@@ -28,7 +29,10 @@ def test_find_user_api_key_env(tmpdir):
     with tmpdir.as_cwd():
         with open(".opentopography.txt", "w") as fp:
             fp.write("bar")
-    assert find_user_api_key() == "foo"
+        key = ApiKey.from_sources()
+    assert key == "foo"
+    assert key.source == "env"
+    assert key == ApiKey("foo", source="env")
 
 
 @mock.patch.dict(os.environ, {"OPENTOPOGRAPHY_API_KEY": "foo"})
@@ -40,13 +44,16 @@ def test_find_user_api_key_from_file(tmpdir):
             fp.write("bar")
 
         with mock.patch.dict(os.environ, env, clear=True):
-            assert find_user_api_key() == "bar"
+            key = ApiKey.from_sources()
+            assert key == "bar"
+            assert key.source.startswith("file:")
+            assert key.source.endswith(".opentopography.txt")
 
 
 def test_read_first_missing(tmpdir):
     with tmpdir.as_cwd():
-        assert read_first_of(["foo.txt"]) == ""
-        assert read_first_of([]) == ""
+        assert find_first_of(["foo.txt"]) is None
+        assert find_first_of([]) is None
 
 
 def test_read_first_file(tmpdir):
@@ -56,16 +63,16 @@ def test_read_first_file(tmpdir):
         with open("bar.txt", "w") as fp:
             fp.write("bar")
 
-        assert read_first_of(["foo.txt", "bar.txt"]) == "foo"
-        assert read_first_of(["bar.txt", "foo.txt"]) == "bar"
+        assert find_first_of(["foo.txt", "bar.txt"]).name == "foo.txt"
+        assert find_first_of(["bar.txt", "foo.txt"]).name == "bar.txt"
 
 
 def test_use_demo_key_is_a_string():
-    demo_key = use_demo_key()
-    assert isinstance(demo_key, str)
-    assert len(demo_key) > 0
+    key = ApiKey.from_demo()
+    assert len(key) > 0
+    assert key.source == "demo"
 
 
 def test_use_demo_key_issues_warning():
     with pytest.warns(UserWarning):
-        use_demo_key()
+        ApiKey.from_demo()

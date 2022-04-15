5aa203e8d36440d1567cd1c368a9eadf7c6a0b83
mcflugen <mcflugen@gmail.com>
2022-04-15 09:19:27 -0600

    add module for bmi-topography errors

---
+++ bmi_topography/errors.py
@@ -0,0 +1,19 @@
+class BmiTopographyError(Exception):
+
+    """Base exception for all bmi_topography errors."""
+
+    pass
+
+
+class MissingKeyError(BmiTopographyError):
+
+    """Raise if an API key cannot be found."""
+
+    pass
+
+
+class BadKeyError(BmiTopographyError):
+
+    """Raise for an invalid key."""
+
+    pass
